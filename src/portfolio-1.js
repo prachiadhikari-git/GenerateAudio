b66c4a3d6c53aae300a9cb0880f45f5f542e6de0
prachi <prachi.adhikari@gmail.com>
2025-10-27 22:49:15 +0530

    mod

---
+++ src/portfolio-1.js
@@ -0,0 +1,4 @@
+import {createDialogue} from './utils.js'
+
+const morningText = '[smiling] Your portfolio has kind of cool mix. You have global equities, crypto, and digital real estate'
+await createDialogue (morningText, "portfolio-1.mp3");
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 
-
 import { ElevenLabsClient, play } from '@elevenlabs/elevenlabs-js';
 import {createWriteStream} from 'fs';
 
@@ -15,20 +14,19 @@ const elevenlabs = new ElevenLabsClient({
 //     outputFormat: 'mp3_44100_128', // output_format
 //   }
 // );
-
+export async function createDialogue (inputText, outputFile) {
 const audio = await elevenlabs.textToDialogue.stream({
   inputs: [
     {
-        text: '[cheerfully] Hey Vick! Morning! hmm.......',
-        voiceId: 'JBFqnCBsd6RMkjVDRZzb', // voice_id
+        text: inputText,
+        voiceId: 'cgSgspJ2msm6clMCkdW9', // voice_id
         outputFormat: 'mp3_44100_128',
   
     },
   ],
 });
 
-
-const fileName = "./morning.mp3"
+const fileName = outputFile
 
 const fileWriteStream = createWriteStream(fileName)
 
@@ -37,6 +35,8 @@ for await (const chunk of audio) {
         fileWriteStream.write(chunk);
 }
 fileWriteStream.end();
-console.log('Audio saved to output.mp3');
-// audio.pipeTo(fileStream)
-// await play(audio);
+console.log('Audio saved to mp3');
+}
+
+
+

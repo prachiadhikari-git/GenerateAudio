2f07211cda642d3a40de81feceb342ab8e480146
prachiadhikari-git <56343554+prachiadhikari-git@users.noreply.github.com>
2025-10-28 14:58:36 +0530

    Add files via upload

---
+++ src/portfolio-4.js
@@ -0,0 +1,11 @@
+import { ElevenLabsClient, play } from '@elevenlabs/elevenlabs-js';
+import {createWriteStream} from 'fs';
+import {createDialogue} from './utils.js'
+
+const elevenlabs = new ElevenLabsClient({
+    apiKey: "sk_2c17725a936248a92285ccb00ffaa957034e934fa8f4cab3", // Defaults to process.env.ELEVENLABS_API_KEY
+});
+
+
+const morningText = '[smiling] ok... cool, let’s go to your goals'
+await createDialogue (morningText, "portfolio-3.mp3");

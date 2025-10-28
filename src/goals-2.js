444e2a230112c0c62735ba3c0b67346c8c8b94a8
prachiadhikari-git <56343554+prachiadhikari-git@users.noreply.github.com>
2025-10-28 15:00:53 +0530

    Add files via upload

---
+++ src/goals-2.js
@@ -0,0 +1,13 @@
+
+
+import { ElevenLabsClient, play } from '@elevenlabs/elevenlabs-js';
+import {createWriteStream} from 'fs';
+import {createDialogue} from './utils.js'
+
+const elevenlabs = new ElevenLabsClient({
+    apiKey: "sk_2c17725a936248a92285ccb00ffaa957034e934fa8f4cab3", // Defaults to process.env.ELEVENLABS_API_KEY
+});
+
+
+const morningText = 'When do you want to discuss, today or tomorrow?'
+await createDialogue (morningText, "goals2.mp3");

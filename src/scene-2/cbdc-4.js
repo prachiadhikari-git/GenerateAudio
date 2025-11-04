8f7d7ce6bd5928d33b5648a97d95d8ac59007017
prachi <prachi.adhikari@gmail.com>
2025-11-04 16:36:11 +0530

    audio

--- src/scene-2/cbdc-4.js
+++ src/scene-2/cbdc-4.js
@@ -1,11 +1,11 @@
 import { ElevenLabsClient, play } from '@elevenlabs/elevenlabs-js';
 import {createWriteStream} from 'fs';
-import {createDialogue} from './utils.js'
+import {createDialogue} from '../utils.js'
 
 const elevenlabs = new ElevenLabsClient({
     apiKey: "sk_2c17725a936248a92285ccb00ffaa957034e934fa8f4cab3", // Defaults to process.env.ELEVENLABS_API_KEY
 });
 
 
-const morningText = '[smiling]Hmm..She is a Golden Retriever and her behavioural description. Please be aware.'
-await createDialogue (morningText, "audio/destination-1.mp3");
+const morningText = '[smiling] ProcesSing C B D C cross border payment..'
+await createDialogue (morningText, "audio-scene-2/cbdc-4.mp3");

389c23c7b1444ad186d0557e468579c2bfda028e
prachi <prachi.adhikari@gmail.com>
2025-11-06 16:20:19 +0530

    mod

--- src/scene-3/rm-1.js
+++ src/scene-3/rm-1.js
@@ -1,4 +1,4 @@
-import {createDialogue} from './utils.js'
+import {createDialogue} from '../utils.js'
 
 const elevenlabs = new ElevenLabsClient({
     apiKey: "sk_2c17725a936248a92285ccb00ffaa957034e934fa8f4cab3", // Defaults to process.env.ELEVENLABS_API_KEY

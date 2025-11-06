77e851322472e995798a96b40b9b1d8d00be84c5
prachi <prachi.adhikari@gmail.com>
2025-11-06 16:26:05 +0530

    add

--- src/goals-1.js
+++ src/goals-1.js
@@ -2,9 +2,7 @@
 
 import {createDialogue} from './utils.js'
 
-const elevenlabs = new ElevenLabsClient({
-    apiKey: "sk_2c17725a936248a92285ccb00ffaa957034e934fa8f4cab3", // Defaults to process.env.ELEVENLABS_API_KEY
-});
+
 
 
 const morningText = 'Your wealth advisor can help you here with different options if you are interested in expediting this goal!'
--- src/scene-3/rm-2.js
+++ src/scene-3/rm-2.js
@@ -1,8 +1,6 @@
 import {createDialogue} from '../utils.js'
 
-const elevenlabs = new ElevenLabsClient({
-    apiKey: "sk_2c17725a936248a92285ccb00ffaa957034e934fa8f4cab3", // Defaults to process.env.ELEVENLABS_API_KEY
-});
+
 
 
 const morningText = 'ok.... then only KYC update is required!'

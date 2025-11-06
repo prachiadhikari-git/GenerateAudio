84f3e4bd120d8537a88e4bdb96a52d16145a2881
prachi <prachi.adhikari@gmail.com>
2025-11-06 16:30:24 +0530

    mod

--- src/scene-3/rm-1.js
+++ src/scene-3/rm-1.js
@@ -2,5 +2,5 @@ import {createDialogue} from '../utils.js'
 
 
 
-const morningText = 'hmm... let me check. Sarah are the different things you can help us with?'
+const morningText = 'hmm... let me check. Sara, what are the different things you can help us with?'
 await createDialogue (morningText, "audio-scene-3/rm-1.mp3");

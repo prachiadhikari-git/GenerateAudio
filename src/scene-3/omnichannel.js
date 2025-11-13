a12a0f215a41218c34642fadec9e465e18f49d4f
prachi <prachi.adhikari@gmail.com>
2025-11-13 22:39:35 +0530

    audio

---
+++ src/scene-3/omnichannel.js
@@ -0,0 +1,6 @@
+import {createDialogue} from '../utils.js'
+
+
+
+const morningText = 'Vick, we have reached omnichannel banking space'
+await createDialogue (morningText, "audio-scene-3/omnichannel.mp3");

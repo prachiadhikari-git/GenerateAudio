0b69d2c050ec0359831d9be234244eded0e4d278
prachi <prachi.adhikari@gmail.com>
2025-11-26 12:57:25 +0530

    mod

---
+++ src/appt-1.js
@@ -0,0 +1,9 @@
+
+
+
+import {createDialogue} from './utils.js'
+
+
+
+const morningText = 'You have pet therapy next, would you like to go now?'
+await createDialogue (morningText, "audio/appt-1.mp3");
